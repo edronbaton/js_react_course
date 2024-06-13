9e6d6b0f2bf0db86b6cc78b603100e1ce3ad3fa8
Kriva <edronbaton@proton.me>
2024-06-13 22:32:42 +0200

    lesson_5

---
+++ lesson_5 /script.js
@@ -0,0 +1,17 @@
+let soldier = {
+    healthy: 500, 
+    armor: 100,
+    angry: function () {
+        console.log("Твой папаша-гнида служил во Вьетнаме?!")
+    }
+}
+
+let john = {
+    healthy: 200,
+    seyYes: function () {
+        console.log("Есть! Да, Сэр!")
+    }
+}
+// let john = Object.create(soldier);
+Object.setPrototypeOf(john, soldier);
+console.log(john.angry()); // Твой папаша-гнида служил во Вьетнаме?!
